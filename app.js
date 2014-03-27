e635552270153221915a752e8de83fca7733571f
sts10 <sschlinkert@gmail.com>
2014-03-27 14:03:43 -0400

    checkboxes

--- app.js
+++ app.js
@@ -15,4 +15,25 @@ $(document).ready(function(){
     e.preventDefault();
     $('li').show();
   });
+
+  // $("form#filters").on("change", function(e){
+  //   e.preventDefault();
+
+  // });
+
+  $("#filters :checkbox").click(function() {
+     $("li").hide();
+     $("#filters :checkbox:checked").each(function() {
+
+        $(this) 
+
+         // $("." + $(this).val()).show();
+         console.log($(this)[0]["id"]);
+         var this_checkbox_id = $(this)[0]["id"];
+         
+         $("li[data-location='"+this_checkbox_id+"']").show();
+     });
+  });
+
+
 });
