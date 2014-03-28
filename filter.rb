af8a4941b8e58c3011703fd1fc47086664386fbf
Daniel Kronovet <kronovet@gmail.com>
2014-03-28 12:03:49 -0400

    cleaned up files in coordination with jfiddle

--- filter.rb
+++ filter.rb
@@ -3,9 +3,8 @@ f = [1, 4]
 l = [[1, 2, 4], [2, 3], [1, 3, 4]]
 
 
-def select_lessons(lessons, filters)
+def select_lessons_by_filters(lessons, filters)
   lessons.select do |lesson|
-    puts (lesson & filters == filters)
     lesson & filters == filters
   end
 end
