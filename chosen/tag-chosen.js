2a22aecdf477983226414b952f4eea77691dd97c
sts10 <sschlinkert@gmail.com>
2014-03-28 12:14:58 -0400

    put all chosen files in separate directory

---
+++ chosen/tag-chosen.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function(){
+
+  // chosen config 
+
+  var config = {
+    '.chosen-select'           : {},
+    '.chosen-select-deselect'  : {allow_single_deselect:true},
+    '.chosen-select-no-single' : {disable_search_threshold:10},
+    '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
+    '.chosen-select-width'     : {width:"95%"}
+  }
+  for (var selector in config) {
+    $(selector).chosen(config[selector]);
+  }
+
+
+
+  // our filter logic
+
+
+  $('#tag-select').on('change', function() {
+    $("li.lesson").hide();
+    var filters = getFilters();
+    $(".lesson").each(function(){
+      var $lesson = $(this);
+      var lessonFilters = $lesson.data("filters");
+
+      if(lessonQualified(filters, lessonFilters)){
+        $lesson.show();
+      }
+    });
+  });
+
+  function getFilters(){
+    var filterArray = [];
+    $("#tag-select > option").each(function(){
+      if ($(this).context.index > 0){
+        if(this.selected) {
+          filterArray.push(parseInt($(this).attr("id")));  
+        }
+      }
+    });
+    return filterArray;
+  };
+
+  function lessonQualified(filter, lesson){
+   for(var i = 0; i < filter.length ; i++){
+     if((lesson.indexOf(filter[i])) == -1){
+       return false;
+     }
+   }
+   return true;
+  };
+
+});
