936a50d1af533d38a646c16ffd2f840ea4f90bb9
sts10 <sschlinkert@gmail.com>
2014-03-28 11:51:37 -0400

    filtered search works with chosen

--- tag-chosen.js
+++ tag-chosen.js
@@ -1,17 +1,41 @@
 
 $(document).ready(function(){
 
-  $('#tag-select').on('change', function() {
-    var tags_selected = [];
 
-    $("#tag-select > option").each(function(){
-      if ($(this).context.index > 0){
-        if(this.selected) {
-          tags_selected.push($(this).attr("id"));  
-        }
-      }
-    });
-    console.log(tags_selected);
+   $('#tag-select').on('change', function() {
+
+    $("li.lesson").hide();
+    var filters = getFilters();
+    $(".lesson").each(function(){
+     var $lesson = $(this);
+     var lessonFilters = $lesson.data("filters");
+     console.log($lesson.attr("id"));
+     console.log(lessonFilters);
+     if(lessonQualified(filters, lessonFilters)){
+       $lesson.show();
+     }
+   });
   });
+
+   function getFilters(){
+     var filterArray = [];
+     $("#tag-select > option").each(function(){
+       if ($(this).context.index > 0){
+         if(this.selected) {
+           filterArray.push(parseInt($(this).attr("id")));  
+         }
+       }
+     });
+     return filterArray;
+   };
+
+   function lessonQualified(filter, lesson){
+     for(var i = 0; i < filter.length ; i++){
+       if((lesson.indexOf(filter[i])) == -1){
+         return false;
+       }
+     }
+     return true;
+   };
 
 });
